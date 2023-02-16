e201794f9f68352e998a9e97678b259a5106c5f6
Nikolay Primizenkin <nikolay.primizenkin@NB-PRIMIZENKIN.local>
2023-02-16 23:53:08 +0200

    task 2 added

--- sandbox/src/main/java/Main.java
+++ sandbox/src/main/java/Main.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static double distance(double x1, double x2, double y1, double y2) {
+        double xdif = x1 - x2;
+        double ydif = y1 - y2;
+        return Math.sqrt(Math.pow(xdif, 2) + Math.pow(ydif, 2));
+    }
+
+    public static void main(String[] args) {
+        double x1 = 5.5;
+        double x2 = 9.5;
+        double y1 = 1.5;
+        double y2 = 9.5;
+        double distance = distance(x1, x2, y1, y2);
+        System.out.println("Distance between (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ") is " + distance);
+
+        Point p1 = new Point(5.5, 1.5);
+        Point p2 = new Point(9.5, 9.5);
+        double qwe = Point.distance(p1, p2);
+        System.out.println("Distance between (" + p1.x + ", " + p1.y + ") and (" + p2.x + ", " + p2.y + ") is " + qwe);
+    }
+
 }
--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Point {
+public class Point {
+
+    double x;
+    double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static double distance(Point p1, Point p2) {
+        double xdif = p2.x - p1.x;
+        double ydif = p2.y - p1.y;
+        return Math.sqrt(Math.pow(xdif, 2) + Math.pow(ydif, 2));
+    }
+
 }
