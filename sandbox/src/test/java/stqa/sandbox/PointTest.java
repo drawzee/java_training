cf9eac404aa176d77979a95fdbec5fecbb8713a5
Nikolay Primizenkin <nikolay.primizenkin@NB-PRIMIZENKIN.local>
2023-02-17 22:26:35 +0200

    more tests added

--- sandbox/src/test/java/stqa/sandbox/PointTest.java
+++ sandbox/src/test/java/stqa/sandbox/PointTest.java
@@ -2,7 +2,6 @@ package stqa.sandbox;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import stqa.sandbox.Point;
 
 public class PointTest {
 
@@ -10,7 +9,24 @@ public class PointTest {
     public void distanceTest() {
         Point p1 = new Point(1.5, 2.5);
         Point p2 = new Point(3.5, 3.5);
-        Assert.assertEquals(Point.distance(p1, p2), 2.23606797749979);
+        double distance = Point.distance(p1, p2);
+        Assert.assertEquals(distance, 2.23606797749979);
+    }
+
+    @Test
+    public void notNullTest() {
+        Point p1 = new Point(1.5, 2.5);
+        Point p2 = new Point(3.5, 3.5);
+        double distance = Point.distance(p1, p2);
+        Assert.assertNotNull(distance);
+    }
+
+    @Test
+    public void positiveValueTest() {
+        Point p1 = new Point(1.5, 2.5);
+        Point p2 = new Point(3.5, 3.5);
+        double distance = Point.distance(p1, p2);
+        Assert.assertTrue(distance > 0);
     }
 
 }
