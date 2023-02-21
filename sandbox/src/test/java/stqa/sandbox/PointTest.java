f1ce5c8daed617c884d4f38eb266721f6afdfd2c
Nikolay Primizenkin <nikolay.primizenkin@NB-PRIMIZENKIN.local>
2023-02-21 18:39:34 +0200

    point tests rfactored

--- sandbox/src/test/java/stqa/sandbox/PointTest.java
+++ sandbox/src/test/java/stqa/sandbox/PointTest.java
@@ -9,24 +9,24 @@ public class PointTest {
     public void distanceTest() {
         Point p1 = new Point(1.5, 2.5);
         Point p2 = new Point(3.5, 3.5);
-        double distance = Point.distance(p1, p2);
+        double distance = p1.distance(p2);
         Assert.assertEquals(distance, 2.23606797749979);
     }
 
     @Test
-    public void notNullTest() {
-        Point p1 = new Point(1.5, 2.5);
+    public void zeroCoordinates() {
+        Point p1 = new Point(0.0, 0.0);
         Point p2 = new Point(3.5, 3.5);
-        double distance = Point.distance(p1, p2);
-        Assert.assertNotNull(distance);
+        double distance = p1.distance(p2);
+        Assert.assertEquals(distance, 4.949747468305833);
     }
 
     @Test
-    public void positiveValueTest() {
-        Point p1 = new Point(1.5, 2.5);
-        Point p2 = new Point(3.5, 3.5);
-        double distance = Point.distance(p1, p2);
-        Assert.assertTrue(distance > 0);
+    public void negativeCoordinates() {
+        Point p1 = new Point(-1.5, -2.5);
+        Point p2 = new Point(-3.5, -3.5);
+        double distance = p1.distance(p2);
+        Assert.assertEquals(distance, 2.23606797749979);
     }
 
 }
