9f47225ccb095d5e9fb607fdff8840cffe92b111
Nikolay Primizenkin <nikolay.primizenkin@NB-PRIMIZENKIN.local>
2023-03-04 21:20:22 +0200

    typo fixed

--- sandbox/src/main/java/stqa/sandbox/Collections.java
+++ sandbox/src/main/java/stqa/sandbox/Collections.java
@@ -6,9 +6,9 @@ import java.util.List;
 public class Collections {
 
     public static void main(String[] args) {
-        String[] langs = {"Java", "C++", "Python", "Python"};
+        String[] langs = {"Java", "C++", "Python", "Golang"};
 
-        List<String> languages = Arrays.asList("Java", "C++", "Python", "Python");
+        List<String> languages = Arrays.asList("Java", "C++", "Python", "Golang");
 
         for (String l : languages) {
             System.out.println("I'm picking " + l);
