268d7bbfd0aa3541c67a13dc3297ac2e47cf7dc7
Nikolay Primizenkin <nikolay.primizenkin@NB-PRIMIZENKIN.local>
2023-03-14 19:17:43 +0200

    unused import removed

--- address-book-web-tests/src/test/java/stqa/addressbook/appmanager/AppManager.java
+++ address-book-web-tests/src/test/java/stqa/addressbook/appmanager/AppManager.java
@@ -2,7 +2,6 @@ package stqa.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.Browser;
