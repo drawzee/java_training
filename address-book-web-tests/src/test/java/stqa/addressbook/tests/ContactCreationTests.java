c4e0b7fd5770a71efb3ae44dd841b361985d6a16
Nikolay Primizenkin <nikolay.primizenkin@NB-PRIMIZENKIN.local>
2023-03-29 00:20:51 +0300

    contact creation test migrated to data provider

--- address-book-web-tests/src/test/java/stqa/addressbook/tests/ContactCreationTests.java
+++ address-book-web-tests/src/test/java/stqa/addressbook/tests/ContactCreationTests.java
@@ -1,34 +1,77 @@
 package stqa.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
 import org.openqa.selenium.By;
+import org.openqa.selenium.json.TypeToken;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import stqa.addressbook.model.ContactData;
 import stqa.addressbook.model.Contacts;
 import stqa.addressbook.model.GroupData;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.*;
 
 public class ContactCreationTests extends TestBase {
 
+    @DataProvider
+    public Iterator<Object[]> contactsJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null) {
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {}.getType()); //List<ContactData>.class
+        return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator(); //Wrapping object in an array
+    }
+
+    @DataProvider
+    public Iterator<Object[]> contactsXml() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+        String xml = "";
+        String line = reader.readLine();
+        while (line != null) {
+            xml += line;
+            line = reader.readLine();
+        }
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        xstream.allowTypes(new Class[]{ContactData.class});
+        List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+        return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
+    }
+
     @BeforeMethod
     public void checkPreconditions() {
         app.session().login("admin", "secret");
+        /*
         app.goTo().groupPage();
         if (!app.group().exists()) {
             app.group().create(new GroupData().withName("Test").withHeader("Test header").withFooter("Test footer"));
         }
+         */
     }
 
-    @Test
-    public void testAddContactTests() {
-        app.goTo().groupPage();
-        String CurrentGroup = app.wd.findElement(By.className("group")).getText();
+    @Test(dataProvider = "contactsXml")
+    public void testAddContactTests(ContactData contact) {
+        //app.goTo().groupPage();
+        //String CurrentGroup = app.wd.findElement(By.className("group")).getText();
         app.goTo().homePage();
         Contacts initialContacts = app.contact().all();
+        /*
         File photo = new File("src/test/resources/contact_photo.png");
         ContactData contact = new ContactData()
                 .withFirstName("Test")
@@ -38,6 +81,7 @@ public class ContactCreationTests extends TestBase {
                 .withEmail("email@test.com")
                 .withGroup(CurrentGroup)
                 .withPhoto(photo);
+         */
         app.contact().create(contact);
         app.goTo().homePage();
         Contacts finalContacts = app.contact().all();
