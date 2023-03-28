9c58bf17b1206241b26e62634cc4bfe96910d1c2
Nikolay Primizenkin <nikolay.primizenkin@NB-PRIMIZENKIN.local>
2023-03-28 21:49:05 +0300

    reading group's attriputes from file added

--- address-book-web-tests/src/test/java/stqa/addressbook/tests/GroupCreationTests.java
+++ address-book-web-tests/src/test/java/stqa/addressbook/tests/GroupCreationTests.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 import stqa.addressbook.model.GroupData;
 import stqa.addressbook.model.Groups;
 
-import java.util.AbstractList;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -16,11 +16,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validGroups() {
+    public Iterator<Object[]> validGroups() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
-        list.add(new Object[]{new GroupData().withName("Test 1").withHeader("Header 1").withFooter("Footer 1")});
-        list.add(new Object[]{new GroupData().withName("Test 2").withHeader("Header 2").withFooter("Footer 2")});
-        list.add(new Object[]{new GroupData().withName("Test 3").withHeader("Header 3").withFooter("Footer 3")});
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+        String line = reader.readLine();
+        while (line != null) {
+            String[] split = line.split(",");
+            list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+            line = reader.readLine();
+        }
         return list.iterator();
     }
 
