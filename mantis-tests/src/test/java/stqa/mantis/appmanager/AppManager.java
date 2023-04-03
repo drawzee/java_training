f3c5293baec8bfe0568eb68c8d8cf0497bf61733
Nikolay Primizenkin <nikolay.primizenkin@NB-PRIMIZENKIN.local>
2023-04-03 19:57:45 +0300

    new module or Mantis tests created

---
+++ mantis-tests/src/test/java/stqa/mantis/appmanager/AppManager.java
@@ -0,0 +1,47 @@
+package stqa.mantis.appmanager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class AppManager {
+
+    private final Properties properties;
+    public WebDriver wd;
+
+    private String browser;
+
+    public AppManager(String browser) {
+        this.browser = browser;
+        properties = new Properties();
+    }
+
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+        System.setProperty("webdriver.gecko.driver", properties.getProperty("driver"));
+
+        switch (browser) {
+            case BrowserType.FIREFOX -> wd = new FirefoxDriver();
+            case BrowserType.CHROME -> wd = new ChromeDriver();
+            case BrowserType.EDGE -> wd = new EdgeDriver();
+            default -> throw new IllegalStateException("Unexpected value: " + browser);
+        }
+
+        wd.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        wd.get(properties.getProperty("web.baseUrl"));
+    }
+
+    public void stop() {
+        wd.quit();
+    }
+
+}
---
+++ mantis-tests/src/test/java/stqa/mantis/tests/TestBase.java
@@ -0,0 +1,22 @@
+package stqa.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import stqa.mantis.appmanager.AppManager;
+
+public class TestBase {
+
+    protected static final AppManager app = new AppManager(System.getProperty("browser", BrowserType.FIREFOX));
+
+    @BeforeSuite
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() throws Exception {
+        app.stop();
+    }
+
+}
